24e1a6564a6da8512908f2027f8ad43e771a4e5b
IsadoraLour <163910452+IsadoraLour@users.noreply.github.com>
2024-11-18 16:51:08 -0300

    Adicionar arquivos de projeto.

---
+++ Conta/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    public abstract class Conta : IConta
+
+    {
+        public string Titular { get; set; }
+        public double Saldo { get; protected set; }
+        public Conta(string titular, double saldoInicial)
+        {
+            Titular = titular;
+            Saldo = saldoInicial;
+        }
+        public virtual void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"{Titular} Depositou {valor:C} Saldo atual: {Saldo:C}");
+        }
+        public virtual bool Sacar(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"{Titular} Sacou {valor:C} Saldo atual: {Saldo:C}");
+                return true;
+            }
+            Console.WriteLine($"{Titular} Tentou sacar {valor:C} saldo é insuficiente");
+            return false;
+        }
+        public void Transferir(double valor, IConta contaDestino)
+        {
+
+            if (Sacar(valor))
+            {
+                contaDestino.Depositar(valor);
+                Console.WriteLine($"{Titular} Transferiu {valor:C} para {((Conta)contaDestino).Titular}");
+            }
+        }
+        public abstract void ExibirInformacoes();
+
+    }
+
+
+}
---
+++ Conta/ContaCorrente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    public class ContaCorrente : Conta
+
+    {
+        public double LimiteDeCredito { get; set; }
+        public ContaCorrente(string titular, double saldoInicial, double limiteDeCredito)
+            : base(titular, saldoInicial)
+        {
+            LimiteDeCredito = limiteDeCredito;
+        }
+        public override bool Sacar(double valor)
+        {
+            if (Saldo + LimiteDeCredito >= valor)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"{Titular} sacou {valor:C} limite de crédito: {Saldo:C}");
+                return true;
+            }
+            Console.WriteLine($"{Titular} tentou sacar {valor:C} passou do saldo.");
+            return false;
+        }
+        public override void ExibirInformacoes()
+        {
+            Console.WriteLine($"Conta Corrente: {Titular} Saldo: {Saldo:C} Limite de Crédito: {LimiteDeCredito:C}");
+        }
+    }
+
+}
